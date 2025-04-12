a8e38da0e0c88bfccb00f4375447ea894b743469
saheks <lolasafasa@gmail.com>
2025-04-12 17:25:50 +0200

    Optimization improvment(19 sec => 0.1 sec). Refactor to different functions. Breakpoints logic improvment

--- solution.c
+++ solution.c
@@ -3,36 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <math.h>
 #include <time.h>
 
 #define STRING_MAX_LENGTH 10000 // the length that can be readed in one iteration
 #define TAB_LENGTH 4 // in some editors the length of tab is different, but normal value is 4 spaces in one tab
 
+int limit_input();
+void formatted_string_output(const char *original_array, unsigned int *array_of_indexes, bool *array_tf, const int amount);
+void find_break(const char *original_array, unsigned int *array_of_indexes, bool *array_tf, unsigned long *amount, const int limit, int *i);
+
 int main() {
-    int chars_limit = 0; // how many chars does the user want to see in one string
-
-    // FILE *fptr = fopen("input.txt", "r");
-
-    {
-        char c; // temporary variable to store getchar() value
-        short res = 0; // to store return value from scanf()
-        char last_char; // to store the \n symbol so the code can know where's the end of string
-        while (1)
-        {
-            printf("Enter how many chars do you want to see in one string: ");
-            res = scanf("%d%c", &chars_limit, &last_char);
-            if (res == EOF)
-            {
-                return 1;
-            }
-            if (chars_limit > 0 && isspace(last_char))
-            {
-                break;
-            }
-            while ((c = getchar()) != '\n' && c != EOF); // cleaning buffer if scanf didn't take all of values
-        }
-    }
+    const int chars_limit = limit_input(); // how many chars does the user want to see in one string
 
     char *str = malloc(sizeof(char) * STRING_MAX_LENGTH); // allocating memory for string array
     if (str == NULL)
@@ -76,10 +57,7 @@ int main() {
             }
         } else // if string was smaller then chars_limit program can just print the original string
         {
-            for (size_t i = 0; str[i] != '\0'; i++)
-            {
-                printf("%c", str[i]);
-            }
+            formatted_string_output(str, NULL, NULL, 0);
             continue;
         }
         
@@ -89,16 +67,15 @@ int main() {
         str[strlen(str) - 1] = '\0';
         */
        
-        unsigned long index_array_index = 0; // iterator
+        unsigned long index_array_iterator = 0; // iterator
         unsigned int length = 0; // length of already checked chars
         bool is_succesful = false; // variable to spot other cycle if the first one worked succesfully
-        index_array[0] = 0; // initializing only first element of index_array because if not it can lead to condition jump
 
         for (unsigned int i = 0; i < str_length; i++)
         {
             if (str[i] == '\t') 
             {
-                length += TAB_LENGTH - (length % TAB_LENGTH); // tabs are not arways one or four spaces, so we need to always check how many spaces
+                length += TAB_LENGTH - (length % TAB_LENGTH); // tabs are not always one or four spaces, so we need to always check how many spaces
                                                               // does the one print
             } else
             {
@@ -107,12 +84,7 @@ int main() {
             
             if (length > chars_limit)
             {
-                if (str[i] == '\n')
-                {
-                    is_succesful = true; // if the char is already \n there's no need to do something
-                }
-                
-                if (index_array_index + 1 > size)
+                if (index_array_iterator + 1 > size)
                 {
                     /*
                     if index_array and copy_or_replace_array are full we reallocate the memory by multipliyng
@@ -125,7 +97,7 @@ int main() {
                         {
                             printf("UNSUCCS");
                             free(str);
-                            free(index_array);
+                            // free(index_array);
                             free(copy_or_replace_array);
                             return 1;
                         }
@@ -138,7 +110,7 @@ int main() {
                             printf("UNSUCCS");
                             free(str);
                             free(index_array);
-                            free(copy_or_replace_array);
+                            // free(copy_or_replace_array);
                             return 1;
                         }
                         copy_or_replace_array = temp; // if succesful store new pointer in old one
@@ -146,92 +118,24 @@ int main() {
                     
                 }
                 
-                
-                bool is_edge = false; // varible to see if cycle is already on the edge and there's no possible characters behind left
-
-                for (unsigned int j = i - 1; j > 0 && !is_succesful; j--)
+                if (str[i] != '\n')
                 {
-                    switch (index_array_index > 0)
-                    {
-                    case true:
-                        if (index_array[index_array_index - 1] == j)
-                        {
-                            is_edge = true;
-                        }
-                        break;
-                    
-                    default:
-                        if (index_array[index_array_index] == j)
-                        {
-                            is_edge = true;
-                        }
-                        break;
-                    }
-
-
-                    if (isspace(str[j]) && !is_succesful && !is_edge)
-                    {
-                        index_array[index_array_index] = j;
-                        copy_or_replace_array[index_array_index] = 1;
-                        i = j;
-                        index_array_index++;
-                        length = 0;
-                        is_succesful = true;
-                        break;
-                    }
-                }
-
-                if (!is_succesful)
-                {
-                    index_array[index_array_index] = i - 2;
-                    i -= 2;
-                    copy_or_replace_array[index_array_index] = 0;
-                    index_array_index++;
+                    find_break(str, index_array, copy_or_replace_array, &index_array_iterator, chars_limit, &i);
                     length = 0;
                 }
-                
-                is_succesful = false;
             }
         }
 
-        
-        for (size_t i = 0; i <= index_array_index - 1; i++)
+        /*
+        for (size_t i = 0; i <= index_array_iterator - 1; i++)
         {
-            printf("amount: %ld position: %d and t/f: %d\n", index_array_index, index_array[i], copy_or_replace_array[i]); // DEBUG
+            printf("amount: %ld position: %d and t/f: %d\n", index_array_iterator, index_array[i], copy_or_replace_array[i]); // DEBUG
         }
-        
-        {
-            int index_array_index_max_possible = index_array_index - 1; // store the amount of stored indexes in array
-            index_array_index = 0;
+        */
 
-            for (size_t i = 0; i < str_length; i++)
-            {
-                if (index_array[index_array_index] == i) // checking if program needs to place \n on this index(or after this index)
-                {
-                    switch (copy_or_replace_array[index_array_index])
-                    {
-                    case true:
-                        printf("\n"); // just place \n on this index (explanation on main:52)
-                        break;
-                    
-                    case false:
-                        printf("%c", str[i]);
-                        printf("\n"); // place \n after this index (explanation on main:52)
-                        break;
-                    }
-                    if (index_array_index + 1 <= index_array_index_max_possible) // if the value was already checked moving in index array futher
-                    {
-                        index_array_index++;
-                    }
-                    
-                    continue;
-                }
-                printf("%c", str[i]);
-                
-            }
-        }
+        formatted_string_output(str, index_array, copy_or_replace_array, index_array_iterator);
 
-        // free(index_array);
+        free(index_array);
         index_array = NULL;
 
         free(copy_or_replace_array);
@@ -245,4 +149,104 @@ int main() {
     printf("\nDone in %.3f seconds\n", elapsed_time);
 
     return 0;
+}
+
+int limit_input()
+{
+    char c; // temporary variable to store getchar() value
+    short res = 0; // to store return value from scanf()
+    char last_char; // to store the \n symbol so the code can know where's the end of string
+    int result = 0;
+    while (1)
+    {
+        printf("Enter how many chars do you want to see in one string: ");
+        res = scanf("%d%c", &result, &last_char);
+        if (res == EOF)
+        {
+            return 1;
+        }
+        if (result > 0 && isspace(last_char))
+        {
+            break;
+        }
+        while ((c = getchar()) != '\n' && c != EOF); // cleaning buffer if scanf didn't take all of values
+    }
+    return result;
+}
+
+void formatted_string_output(const char *original_array, unsigned int *array_of_indexes, bool *array_tf, const int amount)
+{
+    unsigned int array_i = 0;
+    const unsigned int string_length = strlen(original_array);
+
+
+    if (amount < 1)
+    {
+        for (size_t i = 0; i < string_length; i++)
+        {
+            printf("%c", original_array[i]);
+        }
+        return;
+    }
+    
+
+    for (size_t i = 0; i < string_length; i++)
+    {
+        if (array_of_indexes[array_i] == i) // checking if program needs to place \n on this index(or after this index)
+        {
+            switch (array_tf[array_i])
+            {
+            case true:
+                printf("\n"); // just place \n on this index (explanation on main:52)
+                break;
+            
+            case false:
+                printf("%c", original_array[i]);
+                printf("\n"); // place \n after this index (explanation on main:52)
+                break;
+            }
+            
+            if (array_i + 1 <= amount - 1) // if the value was already checked moving in index array futher
+            {
+                array_i++;
+            }
+            
+            continue;
+        }
+        printf("%c", original_array[i]);
+    }
+}
+
+void find_break(const char *original_array, unsigned int *array_of_indexes, bool *array_tf, unsigned long *amount, const int limit, int *i)
+{
+    bool is_succesful = false;
+
+    for (unsigned int j = *i; j > 0 && !is_succesful && j > *i - limit; j--)
+    {
+        // if (*amount > 0)
+        // {
+        //     if (array_of_indexes[*amount - 1] == j)
+        //     {
+        //         is_edge = true;
+        //     }
+        // }
+
+        if (isspace(original_array[j]) && !is_succesful)
+        {
+            array_of_indexes[*amount] = j;
+            array_tf[*amount] = 1;
+            *i = j;
+            *amount += 1;
+            is_succesful = true;
+            break;
+        }
+    }
+
+    if (!is_succesful)
+    {
+        array_of_indexes[*amount] = *i - 1;
+        *i -= 1;
+        array_tf[*amount] = 0;
+        *amount += 1;
+    }
 }
